cabc09758318213e6b22c381ebdf8ba23cc739fe
satwiksahoo <satwiksahoojob@gmail.com>
2025-12-30 12:12:28 +0530

    updated code

--- get_lib_version.py
+++ get_lib_version.py
@@ -1,1 +1,12 @@
-# 
+import importlib.metadata
+packages = [
+   
+    "langchain",
+    "langchain-core"
+    ]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
