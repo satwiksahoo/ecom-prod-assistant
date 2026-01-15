eba609c5098622aa3f2f5cba31892127081b9996
satwiksahoo <satwiksahoojob@gmail.com>
2026-01-15 13:09:28 +0530

    aws configured

--- prod_assistant/utils/model_loader.py
+++ prod_assistant/utils/model_loader.py
@@ -22,7 +22,7 @@ class ApiKeyManager:
         self.api_keys = {
             "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
             "GOOGLE_API_KEY": os.getenv("GOOGLE_API_KEY"),
-            "GROQ_API_KEY": os.getenv("GROQ_API_KEY"),
+            "GROQ_API_KEY": os.getenv("GROQ_API_KEY"),  
             "ASTRA_DB_API_ENDPOINT": os.getenv("ASTRA_DB_API_ENDPOINT"),
             "ASTRA_DB_APPLICATION_TOKEN": os.getenv("ASTRA_DB_APPLICATION_TOKEN"),
             "ASTRA_DB_KEYSPACE": os.getenv("ASTRA_DB_KEYSPACE"),
